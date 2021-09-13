c07f5a336103c87e4bc80d78b4746e27f70de5ac
Lev Misyuk <l.misyuk@yandex.ru>
2021-09-13 11:18:48 +0300

    add pytest and config separation

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from app.config import Config
+from app.config import DevConfig
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config.from_object(DevConfig)
 jwt = JWTManager(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
--- app/config.py
+++ app/config.py
@@ -1,8 +1,13 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Config:
+class BaseConfig:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'you-will-never-guess')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///../app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'super-secret')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class TestConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///../test.db')
+
+class DevConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///../app.db')
